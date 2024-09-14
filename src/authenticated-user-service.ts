5176a4a8e15db60c88b032e72ef27e27604c60f5
Adishwar Rishi <adiswa123@gmail.com>
2024-09-14 15:33:53 +1000

    Break all tests with @clerk/backend

--- src/authenticated-user-service.ts
+++ src/authenticated-user-service.ts
@@ -1,4 +1,5 @@
 import { RpcTarget, WorkerEntrypoint } from 'cloudflare:workers';
+import { verifyToken } from '@clerk/backend';
 
 class UserService extends RpcTarget {
 	private externalUserId: string;
@@ -19,6 +20,16 @@ export class AuthenticatedUserService extends WorkerEntrypoint<Env> {
 		if (sessionToken === 'REJECT_ME') {
 			return null;
 		}
+
+		// Comment out the clerk block and see all tests passing
+		const data = await verifyToken(sessionToken, {
+			secretKey: 'MY_SECRET_KEY',
+			authorizedParties: ['http://localhost:5173', 'https://example.com'],
+		});
+		if (data.sub !== externalUserId) {
+			return null;
+		}
+		// clerk block end
 
 		return new UserService(externalUserId, this.env);
 	}
